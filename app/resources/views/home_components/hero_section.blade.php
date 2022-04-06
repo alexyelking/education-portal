80c1bcc223c5d81371d04520a36f93a0ffc7a045
Александр Елькин <newmanforlife@list.ru>
2022-04-06 20:58:13 +0600

    example for sanya

--- app/resources/views/home_components/hero_section.blade.php
+++ app/resources/views/home_components/hero_section.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-7">
                 <div class="hero-text text-white">
-                    <h2>COM-education @lang('content.edcourse')</h2>
+                    <h2>Диплом фальшивка ебанная @lang('content.edcourse')</h2>
                     <p>@lang('content.presentation').</p>
                     <br><br>
                     <p><i>@lang('content.smartphrase')</i></p>
